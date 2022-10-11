635f5705793a3230324b39563c1254555996d766
yoann-fermaud <yoann.fermaud@laplateforme.io>
2022-10-11 18:26:25 +0200

    shell.exe Job06

---
+++ Job06/README.txt
@@ -0,0 +1,4 @@
+Mettre un antislash "\" pour réaliser la multiplication
+ - Exemple : x \* y
+
+NB : ne pas mettre de caractères autres que des chiffres
---
+++ Job06/my_calculator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+case $2 in
+
+    "*")
+      echo 'Result : ' $(($1*$3))
+      ;;
+
+    "+")
+      echo 'Result : ' $(($1+$3))
+      ;;
+
+    "-")
+      echo 'Result : ' $(($1-$3))
+      ;;
+
+    "/")
+      echo 'Result : ' $(($1/$3))
+      ;;
+
+    *)
+      echo 'Opération non valide'
+esac
