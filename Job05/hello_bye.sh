8b4da22f76633b9ebd27c6ca2663e1dbb47c98ec
yoann-fermaud <yoann.fermaud@laplateforme.io>
2022-10-12 19:11:00 +0200

    shell.exe Job05

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [ "$1" = Hello ]
-then
+if [ "$1" = "Hello" ]; then
     echo 'Bonjour, je suis un script'
 
-elif [ "$1" = Bye ]
-then
+elif [ "$1" = "Bye" ]; then
     echo 'Au revoir et bonne journée'
 
 else
