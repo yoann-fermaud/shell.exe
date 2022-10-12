b137ed365b43308cc3ada48180b8417992895143
yoann-fermaud <yoann.fermaud@laplateforme.io>
2022-10-12 19:12:05 +0200

    shell.exe Job06

--- Job06/my_calculator.sh
+++ Job06/my_calculator.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 case $2 in
-
     "*")
-      echo 'Result : ' $(($1*$3))
-      ;;
-
+        echo 'Result : ' $(($1*$3))
+        ;;
     "+")
-      echo 'Result : ' $(($1+$3))
-      ;;
-
+        echo 'Result : ' $(($1+$3))
+        ;;
     "-")
-      echo 'Result : ' $(($1-$3))
-      ;;
-
+        echo 'Result : ' $(($1-$3))
+        ;;
     "/")
-      echo 'Result : ' $(($1/$3))
-      ;;
-
+        echo 'Result : ' $(($1/$3))
+        ;;
     *)
-      echo 'Opération non valide'
+        echo "Opération non valide"
 esac
