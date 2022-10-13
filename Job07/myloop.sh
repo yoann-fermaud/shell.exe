ec902a62db4ac1f6209451e46de2269b86e9490b
yoann-fermaud <yoann.fermaud@laplateforme.io>
2022-10-13 23:14:26 +0200

    shell.exe Job07

---
+++ Job07/myloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for((i=0; i<10; i++)){
+    echo "Je suis une script qui arrive à faire une boucle $(($i+1))"
+}
+
+
+#Another method
+#for((i=0; i<10; i++))
+#do
+#    echo "Je suis une script qui arrive à faire une boucle $(($i+1))"
+#done
